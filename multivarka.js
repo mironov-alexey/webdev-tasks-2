a2671500fbad51a5b4baf156f1c5516364e3c46f
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2016-03-04 12:22:11 +0500

    IIITSSSS IMM-M-M-UTABLE TIME!

--- multivarka.js
+++ multivarka.js
@@ -1,94 +1,123 @@
-'use strict'
+'use strict';
 const MongoClient = require('mongodb').MongoClient;
 const mongoHelper = require('./mongohelper');
+const Immutable = require('immutable');
+
 class Query {
     static server(connectionString) {
         return {
-            collection: collection => new Query(connectionString, collection)
-        }
+            collection: collection => new Query({
+                connectionString,
+                collection
+            })
+        };
     }
-    constructor (connectionString, collection) {
-        this.connectionString = connectionString;
-        this.collection = collection;
-        this.queryObject = {$and:[{}]};
-        this.dbFunction = undefined;
-        this.field = undefined;
-        this.isNegated = false;
+
+    constructor(query, params) {
+        this.connectionString = query.connectionString;
+        this.collection = query.collection;
+        this.constraints = query.constraints || Immutable.List([{}]);
+        this.queryObject = Immutable.Map({$and: this.constraints});
+        this.field = query.field;
+        this.isNegated = query.isNegated || false;
+        if (params)
+            Object.keys(params).forEach(key => {
+                this[key] = params[key];
+            });
     }
+
     where(field) {
-        this.field = field;
-        this.isNegated = false;
-        return this;
+        return new Query(this, {
+            field,
+            isNegated: false
+        });
     }
+
     not() {
-        this.isNegated = !this.isNegated;
-        return this;
+        return new Query(this, {isNegated: !this.isNegated});
     }
+
     equal(value) {
         return this.addConstraint(mongoHelper
             .constraintFactory
             .equal[Number(this.isNegated)](value));
     }
+
     lessThan(value) {
         return this.addConstraint(mongoHelper
             .constraintFactory
             .lessThan[Number(this.isNegated)](value));
     }
+
     greatThan(value) {
         return this.addConstraint(mongoHelper
             .constraintFactory
             .greatThan[Number(this.isNegated)](value));
     }
+
     include(collection) {
         return this.addConstraint(mongoHelper
             .constraintFactory
             .include[Number(this.isNegated)](collection));
     }
+
     addConstraint(constraint) {
         var subQuery = {};
         subQuery[this.field] = constraint;
-        this.queryObject['$and'].push(subQuery);
-        return this;
+        var constraints = this.constraints.push(subQuery);
+        var queryObject = this.queryObject.set('$and', constraints);
+        return new Query(this, {constraints, queryObject});
     }
+
     find(callback) {
-        this.dbFunction = (collection, cb) =>
-            collection.find(this.queryObject).toArray(cb);
-        this.makeRequest(callback);
-    }
-    remove(callback) {
-        this.dbFunction = (collection, cb) => {
-            collection.deleteMany(this.queryObject, cb);
+        var dbFunction = (collection, cb) => {
+            collection.find(mapToObject(this.queryObject)).toArray(cb);
         };
-        this.makeRequest(callback);
+        this.makeRequest(dbFunction, callback);
     }
+
+    remove(callback) {
+        var dbFunction = (collection, cb) => {
+            collection.deleteMany(mapToObject(this.queryObject), cb);
+        };
+        this.makeRequest(dbFunction, callback);
+    }
+
     insert(record, callback) {
-        this.dbFunction = (collection, innerCallback) => {
+        var dbFunction = (collection, innerCallback) => {
             collection.insertOne(record, innerCallback);
         };
-        this.makeRequest(callback);
+        this.makeRequest(dbFunction, callback);
     }
+
     set(newField, value) {
         var setObject = {};
         setObject[newField] = value;
-        this.setQuery = {$set: setObject};
-        return this;
+        return new Query(this, {setQuery: {$set: setObject}});
     }
+
     update(callback) {
-        this.dbFunction = (collection, cb) => {
-            collection.updateMany(this.queryObject, this.setQuery, cb);
+        var dbFunction = (collection, cb) => {
+            collection.updateMany(mapToObject(this.queryObject), this.setQuery, cb);
         };
-        this.makeRequest(callback);
+        this.makeRequest(dbFunction, callback);
     }
-    makeRequest (callback) {
-        var query = this;
+
+    makeRequest(dbFunction, callback) {
+        var collection = this.collection;
         MongoClient.connect(this.connectionString, function (err, db) {
-            var collection = db.collection(query.collection);
-            query.dbFunction(collection, function (err, result) {
+            var dbCollection = db.collection(collection);
+            dbFunction(dbCollection, function (err, result) {
                 callback(err, result);
                 db.close();
             });
         });
     }
 }
-
+function mapToObject(map) {
+    return [...map.keys()].reduce((object, key) => {
+        object[key] = map.get(key).toArray();
+        return object;
+    }, {});
+}
 module.exports.server = Query.server;
--- test.js
+++ test.js
@@ -1,17 +1,22 @@
-const MongoClient = require('mongodb').MongoClient;
+'use strict';
 const multivarka = require('./multivarka');
 /*
-    { "_id" : ObjectId("56d72ffcf800d9253a4ca97e"), "a" : 1, "b" : 3, "c" : 11 }
-    { "_id" : ObjectId("56d7301bf800d9253a4ca97f"), "a" : 1, "b" : 3, "c" : 9 }
-    { "_id" : ObjectId("56d73024f800d9253a4ca980"), "a" : 1, "b" : 1, "c" : 9 }
-    { "_id" : ObjectId("56d7302df800d9253a4ca981"), "a" : 2, "b" : 1, "c" : 8 }
-    { "_id" : ObjectId("56d730d1f800d9253a4ca982"), "a" : 1, "b" : 3, "c" : 4 }
-    { "_id" : ObjectId("56d730daf800d9253a4ca983"), "a" : 1, "b" : 3, "c" : 2 }
-    { "_id" : ObjectId("56d730dcf800d9253a4ca984"), "a" : 1, "b" : 3, "c" : 3 }
-    { "_id" : ObjectId("56d730e0f800d9253a4ca985"), "a" : 1, "b" : 3, "c" : 5 }
+ [ a: 1, b: 3, c: 11 },
+ { a: 1, b: 3, c: 9 },
+ { a: 1, b: 1, c: 9 },
+ { a: 2, b: 1, c: 8 },
+ { a: 1, b: 3, c: 4 },
+ { a: 1, b: 3, c: 2 },
+ { a: 1, b: 3, c: 3 },
+ { a: 1, b: 3, c: 5 } ]
 */
 
-var findCb = (err, res) => {
+const variables =
+    multivarka
+        .server('mongodb://localhost/test')
+        .collection('variables');
+
+var onFindCb = (err, res) => {
     if (err) {
         console.log(err);
     } else {
@@ -27,54 +32,46 @@ var onChangeCb = (err, res) => {
     }
 };
 
-test2 = _ => multivarka
-    .server('mongodb://localhost/test')
-    .collection('variables')
-    .where('a').equal(1)
-    .where('b').not().equal(1)
-    .where('c')
-    .include([1, 2, 3, 4, 5, 9])
-    .where('c').not().greatThan(4)
-    .where('c').greatThan(2)
-    .find(findCb);
+var test1 = () =>
+    variables
+        .where('c')
+        .greatThan(3)
+        .where('b')
+        .not()
+        .equal(3)
+        .find(onFindCb);
 
-test1 = _ => multivarka
-    .server('mongodb://localhost/test')
-    .collection('variables')
-    .where('c')
-    .greatThan(3)
-    .where('b')
-    .not()
-    .equal(3)
-    .find();
-insertTest = _ => multivarka
-    .server('mongodb://localhost/test')
-    .collection('variables')
-    .insert({a: 0, b: 0, c: 0}, onChangeCb);
+var test2 = () =>
+    variables
+        .where('a').equal(1)
+        .where('b').not().equal(1)
+        .where('c')
+        .include([1, 2, 3, 4, 5, 9])
+        .where('c').not().greatThan(4)
+        .where('c').greatThan(2)
+        .find(onFindCb);
 
-updateTest = _ => multivarka
-    .server('mongodb://localhost/test')
-    .collection('variables')
-    .where('a')
-    .equal(0)
-    .set('a', -1)
-    .update(onChangeCb);
 
-removeTest = _ => multivarka
-    .server('mongodb://localhost/test')
-    .collection('variables')
-    .where('b')
-    .equal(0)
-    .remove(onChangeCb);
+var insertTest = () => variables.insert({a: 0, b: 0, c: 0}, onChangeCb);
 
-findAll = _ => multivarka
-    .server('mongodb://localhost/test')
-    .collection('variables')
-    .find(findCb);
+var updateTest = () =>
+    variables
+        .where('a')
+        .equal(0)
+        .set('a', -1)
+        .update(onChangeCb);
+
+var removeTest = () =>
+    variables
+        .where('b')
+        .equal(0)
+        .remove(onChangeCb);
+
+var findAll = () => variables.find(onFindCb);
 
 //test1();
 //test2();
-//insertTest();
-//updateTest();
-//removeTest();
-//findAll();
+insertTest();
+setTimeout(updateTest, 300);
+setTimeout(removeTest, 600);
+setTimeout(findAll, 900);
